f9ece5a649c792e70c8ce4ebc04c39cc3f0471e8
nam.nhat <nam.nhat@manaty.net>
2019-12-26 22:26:35 +0700

    apply style and size for tabs

--- demo.js
+++ demo.js
@@ -1,5 +1,7 @@
 import { LitElement, html, css } from "lit-element";
 import "./mv-tab.js";
+import "mv-container";
+import "mv-font-awesome";
 
 export class MvTabDemo extends LitElement {
   static get properties() {
@@ -14,32 +16,138 @@ export class MvTabDemo extends LitElement {
         font-family: var(--font-family, Arial);
         font-size: var(--font-size-m, 10pt);
       }
+      
+      mv-container {
+        --mv-container-min-width: 50%;
+        display: inline-block;
+      }
+      
+      .icon-tab {
+        --mv-tab-padding-bottom: 20px;
+      }
     `;
   }
 
   render() {
     return html`
     <mv-tab group>
-      <mv-tab tab key="html">HTML</mv-tab>
-      <mv-tab content key="html">
-        <p>The most common filename extension for files containing HTML is .html. A common abbreviation of this is .htm,
-          which originated because some early operating systems and file systems, such as DOS and the limitations imposed
-          by FAT data structure, limited file extensions to three letters.</p>
-        <p>There are two axes differentiating various variations of HTML as currently specified: SGML-based HTML versus
-          XML-based HTML (referred to as XHTML) on one axis, and strict versus transitional (loose) versus frameset on the
-          other axis.</p>
-        <p>To understand the subtle differences between HTML and XHTML, consider the transformation of a valid and
-          well-formed XHTML 1.0 document that adheres to Appendix C (see below) into a valid HTML 4.01 document. To make
-          this translation requires the following steps:</p>
-        <p>Those are the main changes necessary to translate a document from XHTML 1.0 to HTML 4.01. To translate from
-          HTML to XHTML would also require the addition of any omitted opening or closing tags. Whether coding in HTML or
-          XHTML it may just be best to always include the optional tags within an HTML document rather than remembering
-          which tags can be omitted.</p>
-        <p>A well-formed XHTML document adheres to all the syntax requirements of XML. A valid document adheres to the
-          content specification for XHTML, which describes the document structure.</p>
+      <mv-tab tab key="perimeter-settings" index=0>Perimeter settings</mv-tab>
+      <mv-tab content key="perimeter-settings" >
+      
+        <mv-container>
+          <mv-tab group>
+            <mv-tab tab key="discovery" index=0 tabType="panel-tab">Discovery</mv-tab>
+            <mv-tab content key="discovery">
+               DISCOVERY
+              <mv-tab group class="icon-tab">
+                <mv-tab tab key="compress" index=0 tabType="icon-tab">
+                  <mv-fa icon="compress-arrows-alt"></mv-fa>
+                </mv-tab>
+                <mv-tab content key="expand">
+                  <p>The most common filename extension for files containing HTML is .html. A common abbreviation of this is .htm,
+                    which originated because some early operating systems and file systems, such as DOS and the limitations imposed
+                    by FAT data structure, limited file extensions to three letters.</p>
+                  <p>There are two axes differentiating various variations of HTML as currently specified: SGML-based HTML versus
+                    XML-based HTML (referred to as XHTML) on one axis, and strict versus transitional (loose) versus frameset on the
+                    other axis.</p>
+                  <p>To understand the subtle differences between HTML and XHTML, consider the transformation of a valid and
+                    well-formed XHTML 1.0 document that adheres to Appendix C (see below) into a valid HTML 4.01 document. To make
+                    this translation requires the following steps:</p>
+                  <p>Those are the main changes necessary to translate a document from XHTML 1.0 to HTML 4.01. To translate from
+                    HTML to XHTML would also require the addition of any omitted opening or closing tags. Whether coding in HTML or
+                    XHTML it may just be best to always include the optional tags within an HTML document rather than remembering
+                    which tags can be omitted.</p>
+                  <p>A well-formed XHTML document adheres to all the syntax requirements of XML. A valid document adheres to the
+                    content specification for XHTML, which describes the document structure.</p>
+                </mv-tab>
+                <mv-tab tab key="expand" index=1 tabType="icon-tab">
+                  <mv-fa icon="expand-arrows-alt"></mv-fa>
+                </mv-tab>
+                <mv-tab content key="compress">
+                  <p>As websites adopt newer code standards that are incompatible with older browsers, these browsers can be cut off
+                    from accessing many of the resources on the web (sometimes intentionally). Many of the most popular sites on the
+                    internet are not just visually degraded on older browsers due to poor CSS support, but do not work at all, in
+                    large part due to the evolution of JavaScript and other web technologies.</p>
+                  <p>As the size of CSS resources used in a project increases, a development team often needs to decide on a common
+                    design methodology to keep them organized. The goals are ease of development, ease of collaboration during
+                    development and performance of the deployed stylesheets in the browser. Popular methodologies include OOCSS
+                    (object oriented CSS), ACSS (atomic CSS), oCSS (organic Cascade Style Sheet), SMACSS (scalable and modular
+                    architecture for CSS), and BEM (block, element, modifier).</p>
+                  <ul>
+                    <li>Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document
+                      written in a markup language like HTML.</li>
+                    <li>CSS is designed to enable the separation of presentation and content, including layout, colors, and fonts.
+                      This separation can improve content accessibility, provide more flexibility and control in the specification
+                      of presentation characteristics, enable multiple web pages to share formatting by specifying the relevant CSS
+                      in a separate .css file, and reduce complexity and repetition in the structural content.</li>
+                    <li>Separation of formatting and content also makes it feasible to present the same markup page in different
+                      styles for different rendering methods, such as on-screen, in print, by voice (via speech-based browser or
+                      screen reader), and on Braille-based tactile devices. CSS also has rules for alternate formatting if the
+                      content is accessed on a mobile device.</li>
+                  </ul>
+                </mv-tab>
+              </mv-tab>
+                
+            </mv-tab>
+            <mv-tab tab key="monitoring" index=1 tabType="panel-tab">Monitoring</mv-tab>
+            <mv-tab content key="monitoring">
+               MONITORING
+              <mv-tab group class="icon-tab">
+                <mv-tab tab key="compress" index=0 tabType="icon-tab">
+                  <mv-fa icon="compress-arrows-alt"></mv-fa>
+                </mv-tab>
+                <mv-tab content key="expand">
+                  <p>The most common filename extension for files containing HTML is .html. A common abbreviation of this is .htm,
+                    which originated because some early operating systems and file systems, such as DOS and the limitations imposed
+                    by FAT data structure, limited file extensions to three letters.</p>
+                  <p>There are two axes differentiating various variations of HTML as currently specified: SGML-based HTML versus
+                    XML-based HTML (referred to as XHTML) on one axis, and strict versus transitional (loose) versus frameset on the
+                    other axis.</p>
+                  <p>To understand the subtle differences between HTML and XHTML, consider the transformation of a valid and
+                    well-formed XHTML 1.0 document that adheres to Appendix C (see below) into a valid HTML 4.01 document. To make
+                    this translation requires the following steps:</p>
+                  <p>Those are the main changes necessary to translate a document from XHTML 1.0 to HTML 4.01. To translate from
+                    HTML to XHTML would also require the addition of any omitted opening or closing tags. Whether coding in HTML or
+                    XHTML it may just be best to always include the optional tags within an HTML document rather than remembering
+                    which tags can be omitted.</p>
+                  <p>A well-formed XHTML document adheres to all the syntax requirements of XML. A valid document adheres to the
+                    content specification for XHTML, which describes the document structure.</p>
+                </mv-tab>
+                <mv-tab tab key="expand" index=1 tabType="icon-tab">
+                  <mv-fa icon="expand-arrows-alt"></mv-fa>
+                </mv-tab>
+                <mv-tab content key="compress">
+                  <p>As websites adopt newer code standards that are incompatible with older browsers, these browsers can be cut off
+                    from accessing many of the resources on the web (sometimes intentionally). Many of the most popular sites on the
+                    internet are not just visually degraded on older browsers due to poor CSS support, but do not work at all, in
+                    large part due to the evolution of JavaScript and other web technologies.</p>
+                  <p>As the size of CSS resources used in a project increases, a development team often needs to decide on a common
+                    design methodology to keep them organized. The goals are ease of development, ease of collaboration during
+                    development and performance of the deployed stylesheets in the browser. Popular methodologies include OOCSS
+                    (object oriented CSS), ACSS (atomic CSS), oCSS (organic Cascade Style Sheet), SMACSS (scalable and modular
+                    architecture for CSS), and BEM (block, element, modifier).</p>
+                  <ul>
+                    <li>Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document
+                      written in a markup language like HTML.</li>
+                    <li>CSS is designed to enable the separation of presentation and content, including layout, colors, and fonts.
+                      This separation can improve content accessibility, provide more flexibility and control in the specification
+                      of presentation characteristics, enable multiple web pages to share formatting by specifying the relevant CSS
+                      in a separate .css file, and reduce complexity and repetition in the structural content.</li>
+                    <li>Separation of formatting and content also makes it feasible to present the same markup page in different
+                      styles for different rendering methods, such as on-screen, in print, by voice (via speech-based browser or
+                      screen reader), and on Braille-based tactile devices. CSS also has rules for alternate formatting if the
+                      content is accessed on a mobile device.</li>
+                  </ul>
+                </mv-tab>
+              </mv-tab>
+              
+            </mv-tab>
+          </mv-tab>
+        </mv-container>
+        
       </mv-tab>
-      <mv-tab tab key="css">CSS</mv-tab>
-      <mv-tab content key="css">
+      <mv-tab tab key="brands-products" index=1>Brands & Products</mv-tab>
+      <mv-tab content key="brands-products">
         <p>As websites adopt newer code standards that are incompatible with older browsers, these browsers can be cut off
           from accessing many of the resources on the web (sometimes intentionally). Many of the most popular sites on the
           internet are not just visually degraded on older browsers due to poor CSS support, but do not work at all, in
@@ -60,30 +168,6 @@ export class MvTabDemo extends LitElement {
             styles for different rendering methods, such as on-screen, in print, by voice (via speech-based browser or
             screen reader), and on Braille-based tactile devices. CSS also has rules for alternate formatting if the
             content is accessed on a mobile device.</li>
-        </ul>
-      </mv-tab>
-      <mv-tab tab key="javascript" disabled>Javascript</mv-tab>
-      <mv-tab content key="javascript">
-        <ul>
-          <li>So, along with Macromedia, work restarted on ECMAScript 4 with the goal of standardizing what was in
-            ActionScript 3. To this end, Adobe Systems released the ActionScript Virtual Machine 2, code named Tamarin, as
-            an open source project. But Tamarin and ActionScript 3 were too different from web JavaScript to converge, as
-            was realized by the parties in 2007 and 2008.</li>
-          <li>Alas, there was still turmoil between the various players; Douglas Crockford—then at Yahoo!—joined forces
-            with Microsoft in 2007 to oppose ECMAScript 4, which led to the ECMAScript 3.1 effort. The development of
-            ECMAScript 4 was never completed, but that work influenced subsequent versions.</li>
-          <li>In July 2008, the disparate parties on either side came together in Oslo. This led to the eventual agreement
-            in early 2009 to rename ECMAScript 3.1 to ECMAScript 5 and drive the language forward using an agenda that is
-            known as Harmony. ECMAScript 5 was finally released in December 2009.</li>
-          <li>In June 2011, ECMAScript 5.1 was released to fully align with the third edition of the ISO/IEC 16262
-            international standard. ECMAScript 2015 was released in June 2015. ECMAScript 2016 was released in June 2016.
-            The current version is ECMAScript 2017, released in June 2017.</li>
-          <li>JavaScript has become one of the most popular programming languages on the Web. However, many professional
-            programmers initially denigrated the language due to the perceived target audience of Web authors and other
-            such "amateurs". The advent of Ajax returned JavaScript to the spotlight and brought more professional
-            programming attention. The result was a proliferation of comprehensive frameworks and libraries, improved
-            JavaScript programming practices, and increased usage of JavaScript outside Web browsers, as seen by the
-            proliferation of Server-side JavaScript platforms.</li>
         </ul>
       </mv-tab>
     </mv-tab>
--- mv-tab.js
+++ mv-tab.js
@@ -9,7 +9,9 @@ export class MvTab extends LitElement {
       content: { type: Boolean, attribute: true },
       disabled: { type: Boolean, attribute: true },
       active: { type: Boolean, reflect: true },
-      selected: { type: String, reflect: true }
+      selected: { type: String, reflect: true },
+      index: { type: Number, attribute: true },
+      tabType: { type: String, attribute: true }
     };
   }
 
@@ -20,58 +22,175 @@ export class MvTab extends LitElement {
 				font-size: var(--font-size-m, 10pt);
 				line-height: var(--line-height-s, 1.625);
 			}
+			
 			ul {
 				list-style: none;
 				padding: 0;
-				margin: 0;
+				margin: 0 auto;
+				display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        padding-bottom: var(--mv-tab-padding-bottom, 12px);
+        position: relative;
+        width: var(--mv-tab-width, 100%);
 			}
+			
 			li {
-				cursor: pointer;
-				display: inline;
-				padding: 3px 3px 1px 3px;
-				margin-left: 5px;
-				margin-right: 5px;
-				border-radius: 15% 15% 0 0;
-				border: 1px solid var(--nd-color, rgb(71, 74, 75));
-				border-bottom: none;
-				background-color: var(--on-pdd-color, rgb(202, 213, 218));
+				display: flex;
+				align-items: center;
+        justify-content: center;
+        white-space: nowrap;
+        cursor: pointer; 
 			}
-			li[active] {
-				background-color: var(--b-color, rgb(255, 255, 255));
-				padding-bottom: 1px;
-				border-bottom: 1px solid var(--b-color, rgb(255, 255, 255));
-			}
+			
 			li[disabled] {
 				cursor: unset;
 				color: var(--n-color, rgb(105, 109, 110));
 				background-color: var(--on-pl-color, rgb(187, 187, 187));
 				border-color: var(--nd-color, rgb(187, 187, 187));
 			}
+			
 			section {
-				border: 1px solid black;
 				padding: 10px;
 				background-color: var(--b-color, rgb(255, 255, 255));
 			}
+			
 			p {
 				display: none;
 				margin-block-start: unset;
 				margin-block-end: unset;
 			}
+			
+			ul.main-tab {
+			  position: relative;
+			}
+			
+			li.main-tab {
+			  font-size: var(--mv-tab-font-size, 20px);
+        font-weight: 400;
+        height: var(--mv-tab-height, 71px);
+        box-shadow: inset 0 0 9px 0 rgba(29, 155, 201, 0.3);
+			  background: var(--mv-tab-background-color, #DFE7EE);
+			  color: var(--mv-tab-color, #67AFD3);
+			  border: 1px solid #99D2E7;
+			  box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+			}
+			
+			li.main-tab:hover {
+			  box-shadow: inset 0 0 30px 0 rgba(0, 0, 0, 0.3);
+			}
+			
+			li[active].main-tab {
+			  font-weight: 700;
+        box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.5);
+        color: var(--mv-tab-color-active, #FFFFFF);
+			}
+			
+			li[index="0"].main-tab {
+			  border-top-left-radius: 5px;
+        border-bottom-left-radius: 5px;
+			}
+			
+		  li[index="0"][active].main-tab {
+		    background: var(--mv-tab-background-color-active, linear-gradient(to right, #007adf 0%, #00ec76 180%));
+		    border: none;
+			}
+			
+			li[index="1"].main-tab {
+			  border-top-right-radius: 5px;
+        border-bottom-right-radius: 5px;
+			}
+			
+			li[index="1"][active].main-tab {
+			  background: var(--mv-tab-background-color-active, linear-gradient(to left, #007adf 0%, #00ec76 180%));
+			  border: none;
+			}
+			
+			ul.panel-tab {
+			  position: relative;
+			}
+			
+			li.panel-tab {
+			  font-size: var(--mv-tab-font-size, 20px);
+        font-weight: 400;
+        height: var(--mv-tab-height, 51px);
+        color: var(--mv-tab-color, #4E686D);
+			  border: solid 1px var(--mv-tab-background-color-active, #4E686D);
+			  background: var(--mv-tab-background-color, #FFFFFF);
+			  box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+			}
+			
+			li.panel-tab:hover {
+			  box-shadow: inset 0 0 30px 0 rgba(0, 0, 0, 0.3);
+			}
+			
+			li[index="0"].panel-tab {
+			  border-radius: 25px 0 0 25px;
+			}
+			
+			li[index="1"].panel-tab {
+			  border-radius: 0 25px 25px 0;
+			}
+			
+			li[active].panel-tab {
+			  background: var(--mv-tab-background-color-active, #4E686D);
+			  color: var(--mv-tab-color-active, #FFFFFF);
+			  box-shadow: none;
+			}
+			
+			ul.icon-tab {
+			  position: relative;
+			}
+			
+			li.icon-tab {
+        border-radius: 5px;
+        width: var(--mv-tab-icon-width, 29px);
+        height: var(--mv-tab-icon-height, 29px);
+        background-color: var(--mv-tab-background-color, #FFFFFF);
+        color: var(--mv-tab-color, #4E686D);
+        border: 1px solid var(--mv-tab-background-color-active, #4E686D);
+        font-size: var(--mv-tab-font-size, 16px);
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+			}
+			
+			li.icon-tab:hover {
+			  box-shadow: inset 0 0 5px 0 rgba(0, 0, 0, 0.4);
+			}
+			
+			li[active].icon-tab {
+			  background: var(--mv-tab-background-color-active, #4E686D);
+			  color: var(--mv-tab-color-active, #FFFFFF);
+			  box-shadow: none;
+			}
+			
+			li[index="0"].icon-tab {
+			  position: absolute;
+			  right: 39px;
+			}
+			
+			li[index="1"].icon-tab {
+			  position: absolute;
+			  right: 0px;
+			}
 		`;
   }
 
-  selectTab = () => {
-    if (!this.disabled) {
-      this.parentNode.selected = this.key;
-    }
-  };
+  constructor() {
+    super();
+    this.tabType = "main-tab";
+  }
 
   render() {
     if (this.group) {
       return html`
         <div class="mv-tab-group">
           <nav class="mv-tab-menu">
-            <ul>
+            <ul class="${this.tabType}">
               <slot name="tab-item"></slot>
             </ul>
           </nav>
@@ -84,7 +203,9 @@ export class MvTab extends LitElement {
       this.setAttribute("slot", "tab-item");
       return html`
       <li
+        class="${this.tabType}"
         .value="${this.key}"
+        index="${this.index}"
         ?disabled="${this.disabled}"
         ?active="${this.active}"
         @click="${this.selectTab}"
@@ -140,6 +261,12 @@ export class MvTab extends LitElement {
     }
     super.attributeChangedCallback(name, oldval, newval);
   }
+
+  selectTab = () => {
+    if (!this.disabled) {
+      this.parentNode.selected = this.key;
+    }
+  };
 }
 
 customElements.define("mv-tab", MvTab);
